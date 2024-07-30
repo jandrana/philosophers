feffb09f038b095c3319bf16f87be1568f8e2647
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-30 20:41:51 +0200

    Added new error.c file with error handler

---
+++ philo/src/utils/error.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 16:21:41 by ana-cast          #+#    #+#             */
+/*   Updated: 2024/07/30 20:37:03 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo.h>
+
+// NOT USED FOR NOW
+void	exit_program_nl(int exit_code)
+{
+	printf("\n");
+	exit(exit_code);
+}
+
+// NOT USED ANYMORE (NOW USING FPRINTF, CHECK WITH STAFF IF ALLOWED)
+void	putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (!s)
+		write(fd, "(null)", 6);
+	else
+	{
+		i = ft_strlen(s);
+		write(fd, s, i);
+	}
+}
+
+void	put_badarg(int bad_arg)
+{
+	if (bad_arg == 1)
+		fprintf(stderr, "[number_of_philosophers]");
+	if (bad_arg == 2)
+		fprintf(stderr, "[time_to_die]");
+	if (bad_arg == 3)
+		fprintf(stderr, "[time_to_eat]");
+	if (bad_arg == 4)
+		fprintf(stderr, "[time_to_sleep]");
+	if (bad_arg == 5)
+		fprintf(stderr, "[times_each_philosopher_must_eat]");
+}
+
+void	*put_error(int error, char *str, int bad_arg)
+{
+	fprintf(stderr, "\t%s%s-philo: Error: %s", RED, BOLD, WHITE);
+	if (error == E_NARGS)
+		fprintf(stderr, "invalid number of arguments\n");
+	if (error == E_NOTPOS || error == E_NEG)
+	{
+		put_badarg(bad_arg);
+		fprintf(stderr, "=[%s] ", str);
+		if (error == E_NEG)
+			fprintf(stderr, "must be a number greater than 0\n");
+		else
+			fprintf(stderr, "must be a positive number\n");
+	}
+	else if (error == E_NOMEM)
+		fprintf(stderr, "Out of memory\n");
+	return (NULL);
+}
