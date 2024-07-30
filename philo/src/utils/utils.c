b52811c939ade4b74f1f0402dc507dd7a5a23f1f
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-30 20:46:34 +0200

    Added utils.c file with general util functions

---
+++ philo/src/utils/utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 16:11:50 by ana-cast          #+#    #+#             */
+/*   Updated: 2024/07/30 20:46:21 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo.h>
+
+int	ft_strlen(const char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (s1[j])
+	{
+		dest[j] = s1[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*substr;
+	unsigned int	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = (unsigned int)ft_strlen((char *)s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (start + len > s_len)
+		len = s_len - start;
+	substr = (char *)malloc(sizeof(char) * len + 1);
+	if (!substr)
+		return (NULL);
+	i = -1;
+	while (++i < (unsigned int)len)
+		substr[i] = s[start + i];
+	substr[i] = '\0';
+	return (substr);
+}
