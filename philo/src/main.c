71f60ee33f5f3af3cc0d8e68c429ff19e0e265a3
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-07-30 20:49:05 +0200

    Implemented parse_input() in main.c

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,8 +6,24 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 18:51:47 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/07/29 18:51:57 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/07/30 20:48:51 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+int	main(int argc, char **argv)
+{
+	t_input	*input;
+
+	input = NULL;
+	if (argc < 2)
+		put_error(E_NARGS, NULL, 0);
+	else if (argc == 2)
+		input = parse_input(argc, ph_split(argv[1], ' '));
+	else
+		input = parse_input(argc, ++argv);
+	if (input)
+		print_input(input);
+	return (0);
+}
