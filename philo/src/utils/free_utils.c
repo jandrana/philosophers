c2fa2936e364864af1c25573356e442bad907c6e
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-05 21:00:51 +0200

    New functions for freeing t_data and destroy mutex

--- philo/src/utils/free_utils.c
+++ philo/src/utils/free_utils.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:10:39 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/08/02 20:22:16 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/08/05 20:36:01 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,36 @@ void	free_array(char ***array)
 			free((*array)[i]);
 		free(*array);
 		*array = NULL;
+	}
+}
+
+void	destroy_philos(t_data *data)
+{
+	int	id;
+
+	id = -1;
+	while (++id < data->input[N_PHILOS])
+		pthread_mutex_destroy(&data->forks[id]);
+	id = -1;
+	while (++id < data->input[N_PHILOS])
+		pthread_mutex_destroy(&data->philos[id].lock);
+}
+
+void	free_data(t_data **data)
+{
+	if (*data)
+	{
+		printf("NUM PHILOSOPHERS:%i\n\n", (*data)->input[N_PHILOS]);
+		if ((*data)->philos && (*data)->forks)
+			destroy_philos(*data);
+		if ((*data)->philos)
+			free((*data)->philos);
+		if ((*data)->forks)
+			free((*data)->forks);
+		if ((*data)->input)
+			free((*data)->input);
+		pthread_mutex_destroy(&(*data)->lock);
+		pthread_mutex_destroy(&(*data)->print);
+		free(*data);
 	}
 }
