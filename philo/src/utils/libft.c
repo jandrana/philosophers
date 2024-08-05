d2cc46a0926064ffc4cf7a727e571041b37500d9
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-05 17:40:27 +0200

    Improved atol data types + return values

--- philo/src/utils/libft.c
+++ philo/src/utils/libft.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 12:44:40 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/08/02 20:23:19 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/08/05 17:40:04 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,32 @@ char	*ft_substr(const char *str, ssize_t start, ssize_t len)
 		return (NULL);
 	philo_strcpy(substr, str + start, len + 1);
 	return (substr);
+}
+
+long long	ft_atoul(char *str)
+{
+	int			i;
+	int			j;
+	long long	result;
+
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	if (str[i] == '-')
+		return (E_NOTPOS);
+	j = i--;
+	while (str[++i] >= '0' && str[i] <= '9' && result >= 0)
+	{
+		result = result * 10 + (str[i] - '0');
+		if ((result > UINT32_MAX))
+			return (E_OORL);
+	}
+	if (str[i] || j == i)
+		return (E_NAN);
+	if (!result)
+		return (E_NOTPOS);
+	return (result);
 }
