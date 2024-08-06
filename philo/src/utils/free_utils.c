67c9bb691d928b9206f40f09b405ab4c8fc305f8
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-06 14:10:09 +0200

    New function for managing exits without leaks

--- philo/src/utils/free_utils.c
+++ philo/src/utils/free_utils.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:10:39 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/08/05 20:36:01 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/08/06 14:10:02 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,31 @@ void	free_data(t_data **data)
 		pthread_mutex_destroy(&(*data)->print);
 		free(*data);
 	}
+}
+
+int	exit_philo(t_data **data, t_input **input, int argc)
+{
+	int	exit_code;
+
+	exit_code = NO_ERROR;
+	if (*input)
+	{
+		exit_code = (*input)->errors;
+		if ((*input)->args && argc == 2)
+			free_array(&(*input)->args);
+		else if (!(*input)->args)
+			exit_code = put_error(NULL, E_NOMEM);
+		free(*input);
+	}
+	else
+		exit_code = put_error(NULL, E_NOMEM);
+	if (exit_code == NO_ERROR)
+	{
+		if (*data)
+			free_data(data);
+		else
+			exit_code = put_error(NULL, E_NOMEM);
+	}
+	return (exit_code);
+
 }
