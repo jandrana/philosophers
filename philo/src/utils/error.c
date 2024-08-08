b2cb525307d703c9287c35a9f2a5fd01dee4abdd
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-08 21:18:23 +0200

    New function for error that are not input related

--- philo/src/utils/error.c
+++ philo/src/utils/error.c
@@ -74,7 +74,7 @@ void	print_output(t_data *data, bool value)
 int	check_parsing(t_data *data)
 {
 	if (!data)
-		return (print_output(NULL, false), E_NOMEM);
+		return (print_error(E_NOMEM, ENOMEM));
 	if (array_len(data->args) < 4 || array_len(data->args) > 5)
 		data->error++;
 	if (!data->error)
@@ -91,13 +91,19 @@ int	check_parsing(t_data *data)
 	return (data->error);
 }
 
-int	print_error(t_type_error type, int errno)
+int	print_error(t_error type, int n_err)
 {
-	if (type == E_NOMEM)
-		return (print_output(NULL, false), E_NOMEM);
+	char	*error_msg;
+
+	if (type == NO_ERROR)
+		return (NO_ERROR);
 	fprintf(stderr, RED BOLD "\nERROR:" WHITE);
 	if (type == E_TIME)
-		fprintf(stderr, OE_TIME ": %s\n", strerror(errno));
+		fprintf(stderr, OE_TIME ": %s\n", strerror(n_err));
+	else
+		error_msg = error_title(type);
+	if (type)
+		fprintf(stderr, "%s" WHITE, error_msg);
 	fprintf(stderr, "\n\n");
-	return (errno);
+	return (n_err);
 }
