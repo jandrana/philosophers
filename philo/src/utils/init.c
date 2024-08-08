65dfc79409849a91453bfea46bec744039b52e59
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-08 21:26:00 +0200

    Updated init_philos with latest structure changes

--- philo/src/utils/init.c
+++ philo/src/utils/init.c
@@ -83,16 +83,19 @@ void	init_philos(t_data	*data)
 
 	num_ph = data->info[N_PHILOS];
 	data->philos = safe_calloc(sizeof(t_philo) * num_ph, &data);
-	malloc_threads(data, num_ph);
+	init_data_mutex(data);
 	i = -1;
 	while (++i < num_ph)
 	{
 		data->philos[i].id = i + 1;
 		data->philos[i].meals = 0;
-		data->philos[i].hunger = 0;
+		data->philos[i].hunger = data->info[T_DIE];
 		data->philos[i].status = THINK;
 		data->philos[i].data = data;
+		data->philos[i].th = 0;
+		data->philos[i].right = &data->forks[i];
+		data->philos[i].left = &data->forks[(i + 1) % num_ph];
+		if (pthread_mutex_init(&data->philos[i].lock, NULL))
+			exit_philo(&data, E_INITMTX);
 	}
-	init_structs_mutex(data);
-	data->start = get_time_ms(0);
 }
