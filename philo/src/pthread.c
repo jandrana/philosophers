3eba3c4e65b39375d20a94814f4a751f4cf9c601
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-12 20:35:54 +0200

    Simplified start_threads()

--- philo/src/pthread.c
+++ philo/src/pthread.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 17:17:43 by ana-cast          #+#    #+#             */
-/*   Updated: 2024/08/12 14:13:20 by ana-cast         ###   ########.fr       */
+/*   Updated: 2024/08/12 20:24:02 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ void	start_threads(t_data *data)
 {
 	int			i;
 	pthread_t	th_sup;
-	t_philo		*philos;
+	void		*exec;
 
-	philos = data->philos;
-	data->start = time_ms(0);
+	exec = get_routine_to_execute(data);
 	i = -1;
-	gettimeofday(&data->t_start, NULL);
 	while (++i < data->info[N_PHILOS])
 	{
-		if (pthread_create(&data->th->p_th[i], NULL, &routine, &philos[i]))
+		if (pthread_create(&data->th->p_th[i], NULL, exec, &data->philos[i]))
 			exit_philo(&data, E_NEWTH);
 	}
-	if (pthread_create(&th_sup, NULL, &check_complete, data))
+	if (pthread_create(&th_sup, NULL, &greed_supervisor, data))
 		exit_philo(&data, E_NEWTH);
 	i = -1;
+	while (!data->start)
+		my_usleep(10);
 	while (++i < data->info[N_PHILOS])
 	{
 		if (pthread_join(data->th->p_th[i], NULL))
