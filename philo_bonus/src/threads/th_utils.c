338e09e9866af425ec27152f5318e787bc727324
Ana Alejandra Castillejo <yo@anaalejandra.com>
2024-08-16 21:09:20 +0200

    Reorganized functions by functionality

--- philo_bonus/src/threads/th_utils.c
+++ philo_bonus/src/threads/th_utils.c
@@ -73,23 +73,3 @@ void	perform_routine(t_philo *philo)
 	sem_post(data->forks);
 	my_usleep(data->info[T_SLEEP], current, data->t_start);
 }
-
-uint64_t	time_ts(struct timeval t_start)
-{
-	struct timeval	time;
-	int				errno;
-	long			sec;
-
-	errno = gettimeofday(&time, NULL);
-	if (errno)
-		return (print_error(E_TIME, errno), 0);
-	sec = time.tv_sec * 1000LL - (t_start.tv_sec * 1000LL);
-	return (sec + (time.tv_usec - t_start.tv_usec) / 1000);
-}
-
-int	my_usleep(t_time sleep, t_time current, struct timeval t_start)
-{
-	while (time_ts(t_start) - current < sleep)
-		usleep(1);
-	return (0);
-}
